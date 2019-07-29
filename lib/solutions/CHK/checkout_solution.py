3152b7e73e87715e396bd1ef90e76644c5e1863d
sbx_user1051 <sbx_user1051@169.254.224.5>
2019-07-29 12:46:01 +0000

    Mon Jul 29 12:46:01 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,34 +1,46 @@
 
 from collections import Counter
-PRICES = {'A':50, 'B':30, 'C':20, 'D':15}
-OFFERED_QUANTITY = {'A':3, 'B':2}
-OFFERED_PRICES = {'A':130, 'B':45}
 
+INVENTORY = {
+    'A': {'price': 50, 'offer': {'cnt': 3, 'price': 120}},
+    'B': {'price': 30, 'offer': {'cnt': 2, 'price': 45}},
+    'C': {'price': 20},
+    'D': {'price': 15}
+}
 
-def get_price(item, offercount, nooffercount):
-    return nooffercount*PRICES[item] + offercount*OFFERED_PRICES[item]
+def get_offer(item):
+    if 'offer' in INVENTORY[item]:
+        return INVENTORY[item]['offer']
+    else:
+        return None
 
-# noinspection PyUnusedLocal
-# skus = unicode string
+def get_price(item, quantity, offer):
+    final_cost = 0
+    if offer:
+        bulk_offer_count = quantity//offer['cnt']
+        bulk_offer_cost = bulk_offer_count * offer['price']
+        quantity = quantity%offer['cnt']
+        final_cost += bulk_offer_cost
+    no_offer_cost = quantity * INVENTORY[item]['price']
+    final_cost += no_offer_cost
+    return final_cost
+
 def checkout(skus):
     if len(skus) == 0:
         return -1
     #check for invalid sku
     for sku in skus:
-        if sku not in PRICES:
+        if sku not in INVENTORY:
             return -1
 
     total_price = 0
-    itemcount = Counter()
-
-    for item in skus:
-        itemcount[item] += 1
-
-    for item in itemcount:
-        offercount = itemcount[item]//OFFERED_QUANTITY[item]
-        nooffercount = itemcount[item]%OFFERED_QUANTITY[item]
-        sameitems_price = get_price(item, offercount, nooffercount)
-        total_price += sameitems_price
+    cart = Counter(skus)
+    for item in cart:
+        quantity = cart[item]
+        offer = get_offer(item)
+        item_price = get_price(item, quantity, offer)
+        total_price += item_price
 
     return total_price
+
 
