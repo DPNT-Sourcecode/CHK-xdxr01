a0bf67e2594427e13c2db8959ac1ec46990abfd5
sbx_user1051 <sbx_user1051@169.254.2.5>
2019-07-29 05:45:40 +0000

    Mon Jul 29 05:45:40 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,29 +1,35 @@
 
 from collections import defaultdict
-product_list = ['A', 'B', 'C', 'D']
-product_price = {'A':50, 'B':30, 'C':20, 'D':15}
-offered_quantity = {'A':3, 'B':2}
-offered_price = {'A':130, 'B':45}
+PRICES = {'A':50, 'B':30, 'C':20, 'D':15}
+OFFERED_QUANTITY = {'A':3, 'B':2}
+OFFERED_PRICES = {'A':130, 'B':45}
+
 
 def get_price(item, offercount, nooffercount):
-    return nooffercount*product_price[item] + offercount*offered_price[item]
+    return nooffercount*PRICES[item] + offercount*OFFERED_PRICES[item]
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    #check for invalid sku
+    for sku in skus:
+        if sku not in PRICES:
+            return -1
+
     total_price = 0
     itemcount = defaultdict(int)
-    
+
     for item in skus:
-        if item in product_list:
+        if item in PRICES:
             itemcount[item] += 1
         else:
             return -1
     for item in itemcount.keys():
-        offercount = itemcount//offered_quantity[item]
-        nooffercount = itemcount%offered_quantity[item]
+        offercount = itemcount//OFFERED_QUANTITY[item]
+        nooffercount = itemcount%OFFERED_QUANTITY[item]
         sameitems_price = get_price(item, offercount, nooffercount)
         total_price += sameitems_price
 
     return total_price
+
 
