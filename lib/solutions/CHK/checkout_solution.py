e128b7413ae07441cfd9030a7339a2e03127544d
sbx_user1051 <sbx_user1051@169.254.224.5>
2019-07-29 13:14:54 +0000

    Mon Jul 29 13:14:54 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,11 +2,18 @@
 from collections import Counter
 
 INVENTORY = {
-    'A': {'price': 50, 'offer': {'cnt': 3, 'price': 130}},
+    'A': {'price': 50, 'offer': [{'cnt': 3, 'price': 130}, {'cnt': 5, 'price': 200}]},
     'B': {'price': 30, 'offer': {'cnt': 2, 'price': 45}},
     'C': {'price': 20},
-    'D': {'price': 15}
+    'D': {'price': 15},
+    'E': {'price': 40, 'free': [{'cnt':2, 'sku': 'A'}]}
 }
+
+def get_free_items(sku):
+    if 'free' in INVENTORY[sku]:
+        return INVENTORY[sku]['free']
+    else:
+        return None
 
 def get_offer(item):
     if 'offer' in INVENTORY[item]:
@@ -25,6 +32,15 @@ def get_price(item, quantity, offer):
     final_cost += no_offer_cost
     return final_cost
 
+def adjust_free_skus(cart):
+    for sku in cart:
+        sku_quantity = sku['cnt']
+        total_free = 0
+        free = get_free_items(sku)
+        for f in free:
+            free_cnt = sku_quantity//f['cnt']
+            total_free += free_cnt
+
 def checkout(skus):
     #check for invalid sku
     for sku in skus:
@@ -33,6 +49,8 @@ def checkout(skus):
 
     total_price = 0
     cart = Counter(skus)
+    cart = adjust_free_skus(cart)
+
     for item in cart:
         quantity = cart[item]
         offer = get_offer(item)
@@ -40,4 +58,5 @@ def checkout(skus):
         total_price += item_price
 
     return total_price
+
 
