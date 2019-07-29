bf290227cff3a3ce8c6140b0cd95bcc722a7ed31
sbx_user1051 <sbx_user1051@169.254.224.5>
2019-07-29 13:35:54 +0000

    Mon Jul 29 13:35:54 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,10 @@ from collections import Counter
 
 INVENTORY = {
     'A': {'price': 50, 'offer': [{'cnt': 3, 'price': 130}, {'cnt': 5, 'price': 200}]},
-    'B': {'price': 30, 'offer': {'cnt': 2, 'price': 45}},
+    'B': {'price': 30, 'offer': {'cnt': 2, 'price': 45}, 'free': {'sku': 'E', 'cnt': 2}},
     'C': {'price': 20},
     'D': {'price': 15},
-    'E': {'price': 40, 'free': [{'cnt':2, 'sku': 'A'}]}
+    'E': {'price': 40}
 }
 
 def get_free_items(sku):
@@ -15,31 +15,36 @@ def get_free_items(sku):
     else:
         return None
 
-def get_offer(item):
+def get_offers(item):
     if 'offer' in INVENTORY[item]:
         return INVENTORY[item]['offer']
     else:
         return None
 
-def get_price(item, quantity, offer):
+def get_price(item, quantity, offers):
     final_cost = 0
-    if offer:
-        bulk_offer_count = quantity//offer['cnt']
-        bulk_offer_cost = bulk_offer_count * offer['price']
-        quantity = quantity%offer['cnt']
-        final_cost += bulk_offer_cost
+    if offers:
+        for offer in offers:
+            bulk_offer_count = quantity//offer['cnt']
+            bulk_offer_cost = bulk_offer_count * offer['price']
+            quantity = quantity%offer['cnt']
+            final_cost += bulk_offer_cost
     no_offer_cost = quantity * INVENTORY[item]['price']
     final_cost += no_offer_cost
     return final_cost
 
 def adjust_free_skus(cart):
+    final_cart = cart
     for sku in cart:
-        sku_quantity = sku['cnt']
-        total_free = 0
         free = get_free_items(sku)
-        for f in free:
-            free_cnt = sku_quantity//f['cnt']
-            total_free += free_cnt
+        if free:
+            other_sku = free['sku']
+            other_sku_offer_cnt = free['cnt']
+            if other_sku in cart:
+                other_sku_cart_cnt = cart[other_sku]
+                this_sku_free_cnt = other_sku_cart_cnt//other_sku_offer_cnt
+                final_cart[sku] -= this_sku_free_cnt
+    return final_cart
 
 def checkout(skus):
     #check for invalid sku
@@ -53,9 +58,10 @@ def checkout(skus):
 
     for item in cart:
         quantity = cart[item]
-        offer = get_offer(item)
-        item_price = get_price(item, quantity, offer)
+        offers = get_offers(item)
+        item_price = get_price(item, quantity, offers)
         total_price += item_price
 
     return total_price
+
 
